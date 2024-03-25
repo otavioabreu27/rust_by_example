93230dbbd752f7ad604dbbcc7fde7b697f513e65
otavioabreu27 <otavio.abreu96@gmail.com>
2024-03-24 21:12:31 -0300

    comment and hello world

---
+++ comments/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // This is an example of a line comment.
+    // There are two slashes at the beginning of the line.
+    // And nothing written after these will be read by the compiler.
+
+    let x = 5 + 90 + 5;
+    println!("Is `x` 10 or 100? x = {}", x);
+}
